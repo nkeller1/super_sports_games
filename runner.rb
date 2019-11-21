b274f325d3f43b0867148edf49463151d875da60
Nathan Keller <keller.nathan@gmail.com>
2019-11-21 15:11:08 -0700

    Loop the game is mostly working but going to try until

--- runner.rb
+++ runner.rb
@@ -3,28 +3,67 @@ require 'minitest/pride'
 require './lib/games'
 require './lib/event'
 
-# @games = Games.new(year)
+@games = Games.new(@year)
 
 puts "Hello. Welcome to Super Sports Games!"
-puts "Please enter the year for the games you want to work with:"
-year = gets.chomp
-puts "Thank You."
-Games.new(year)
-puts "What is the name of the event you wish to create?"
-event_name = gets.chomp
-puts "Thank You."
-puts "What are the ages of the players you wish to create?(Max of 5)"
-player_age_1 = gets.chomp.to_i
-player_age_2 = gets.chomp.to_i
-player_age_3 = gets.chomp.to_i
-player_age_4 = gets.chomp.to_i
-player_age_5 = gets.chomp.to_i
-player_array = [player_age_1, player_age_2, player_age_3, player_age_4, player_age_5]
-puts "Thank You."
-event = Event.new(event_name, player_array)
-puts "Your event is #{event.name.capitalize}"
-puts "Your player ages are #{event.ages}"
-puts "The oldest player is #{event.max_age}."
-puts "The youngest player is #{event.min_age}."
-puts "The average age of your players is #{event.average_age}."
-puts "The standard deviation of your players is #{event.standard_deviation_age}"
+def start
+  puts "Please enter the year for the games you want to work with:"
+  @year = gets.chomp
+  puts "Thank You."
+  puts "What is the name of the event you wish to create?"
+  event_name = gets.chomp.capitalize
+  puts "Thank You."
+  puts "What are the ages of the players you wish to create?(Total of 5)"
+  puts "Player 1"
+  player_age_1 = gets.chomp.to_i
+  puts "Player 2"
+  player_age_2 = gets.chomp.to_i
+  puts "Player 3"
+  player_age_3 = gets.chomp.to_i
+  puts "Player 4"
+  player_age_4 = gets.chomp.to_i
+  puts "Player 5"
+  player_age_5 = gets.chomp.to_i
+  player_array = [player_age_1, player_age_2, player_age_3, player_age_4, player_age_5]
+  puts "Thank You."
+  event = Event.new(event_name, player_array)
+  @game = Games.new(@year)
+  @game.add_event(event)
+  puts "#{@game.summary}"
+  # puts "Do you wish to add another event?(Yes or No)"
+  # another = gets.chomp.first
+  #   if another == "Y"
+  #     start
+  #   else
+  #     break
+  #   end
+  end
+
+  loop do
+    start
+    puts "Do you wish to add another event?(Yes or No)"
+    another = gets.chomp
+    if another == "Yes"
+      start
+    else
+      puts "#{@game.all_events_summary}"
+      break
+    end
+  end
+# puts "Do you wish to add another event?(Yes or No)"
+# another = gets.chomp.first
+# if another == "Y"
+#
+# else
+#   all_event_summery
+# end
+# start
+# all_event_summery = puts "#{game.all_events_summary}"
+
+#
+# puts "Your event is #{event.name.capitalize}"
+# puts "Your player ages are #{event.ages}"
+# puts "The oldest player is #{event.max_age}."
+# puts "The youngest player is #{event.min_age}."
+# puts "The average age of your players is #{event.average_age}."
+# puts "The standard deviation of your players is #{event.standard_deviation_age}"

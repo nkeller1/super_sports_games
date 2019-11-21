331c6fec1b5cc02e49e49bb86fd0a46a5e71cf14
Nathan Keller <keller.nathan@gmail.com>
2019-11-21 14:19:16 -0700

    Command line spits out event details.

--- runner.rb
+++ runner.rb
@@ -13,8 +13,18 @@ Games.new(year)
 puts "What is the name of the event you wish to create?"
 event_name = gets.chomp
 puts "Thank You."
-puts "What are the ages of the players you wish to create?"
-player_ages = gets.chomp.to_a
+puts "What are the ages of the players you wish to create?(Max of 5)"
+player_age_1 = gets.chomp.to_i
+player_age_2 = gets.chomp.to_i
+player_age_3 = gets.chomp.to_i
+player_age_4 = gets.chomp.to_i
+player_age_5 = gets.chomp.to_i
+player_array = [player_age_1, player_age_2, player_age_3, player_age_4, player_age_5]
 puts "Thank You."
-event = Event.new(event_name, player_ages)
-puts "The average age of your players is #{event.average_age}"
+event = Event.new(event_name, player_array)
+puts "Your event is #{event.name.capitalize}"
+puts "Your player ages are #{event.ages}"
+puts "The oldest player is #{event.max_age}."
+puts "The youngest player is #{event.min_age}."
+puts "The average age of your players is #{event.average_age}."
+puts "The standard deviation of your players is #{event.standard_deviation_age}"

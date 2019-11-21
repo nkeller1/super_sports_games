9391d61eb858b9ed79e7c7378fc541bb1d2f4048
Nathan Keller <keller.nathan@gmail.com>
2019-11-21 13:58:49 -0700

    Start of runner

---
+++ runner.rb
@@ -0,0 +1,9 @@
+require 'minitest/autorun'
+require 'minitest/pride'
+require './lib/games'
+require './lib/event'
+
+puts "Hello. Welcome to Super Sports Games!"
+puts "Please enter the year for the games you want to work with:"
+gets.chomp
+puts "Thank You."

5a7adf4d2fa1f26d9385fc434a1b5437781c18b5
Nathan Keller <keller.nathan@gmail.com>
2019-11-21 13:24:53 -0700

    Work on stand dev up to sq rt

--- lib/standard_deviation.rb
+++ lib/standard_deviation.rb
@@ -1,5 +1,23 @@
 ages = [24, 30, 18, 20, 41]
 
-# Your code here for calculating the standard deviation
+sum = ages.sum
+
+count = ages.count
+
+average = (sum.to_f / count.to_f)
+
+sub_array = []
+subtract = ages.each do |age|
+  # require "pry"; binding.pry
+  sub_array << (age - average).round(1)
+end
+
+sq_array = []
+sub_array.each do |num|
+  require "pry"; binding.pry
+  sq_array << Math.sqrt(num)
+end
+
+p sq_array
 
 # When you find the standard deviation, print it out

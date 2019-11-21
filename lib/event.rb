29c3cd82bfd0603f2db57d02b6937a7140b3e350
Nathan Keller <keller.nathan@gmail.com>
2019-11-21 13:45:41 -0700

    Event class up to average age and test written for standard_deviation

--- lib/event.rb
+++ lib/event.rb
@@ -6,5 +6,22 @@ class Event
     @ages = ages
   end
 
+  def max_age
+    @ages.max
+  end
+
+  def min_age
+    @ages.min
+  end
+
+  def average_age
+    sum = @ages.sum
+    count = @ages.count
+    (sum.to_f / count.to_f)
+  end
+
+  def standard_deviation_age
+
+  end
 
 end
